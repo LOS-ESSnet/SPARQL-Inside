3988694606435ed290499b3760fd34d73c2c6723
Nicolas Laval <nicolas.laval@insee.fr>
2018-09-09 15:24:44 +0200

    Update context slide

--- src/js/components/slides/introduction/context.js
+++ src/js/components/slides/introduction/context.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default () => (
-	<div className="centered slide-text">RDF, Triple store, graph</div>
+	<div className="centered slide-text">
+		<div>RDF data are storing in " Triple Store "</div>
+		<br />
+		<div>Triple Stores are divided in " Repositories "</div>
+		<br />
+		<div>Repositories contains RDF data grouped by " graph "</div>
+		<br />
+		<div>
+			<a
+				href="http://graphdb.linked-open-statistics.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				LOS triple store
+			</a>
+		</div>
+	</div>
 );
